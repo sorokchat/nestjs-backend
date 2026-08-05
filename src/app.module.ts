2b2aa472004af6639bd2f3f7f1be4d5b346f71f7
Sorokovsky <Sorokovskys@ukr.net>
2026-08-05 11:10:10 +0300

    'local'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { validate } from './configurations/validation';
 import { UsersModule } from './core/users/users.module';
@@ -6,6 +6,10 @@ import { AuthorizationModule } from './core/authorization/authorization.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeormConfiguration } from './configurations/typeorm.configuration';
 import { JwtModule } from '@nestjs/jwt';
+import { TokensModule } from './core/tokens/tokens.module';
+import { AccessTokenMiddleware } from './core/authorization/access-token.middleware';
+import { ChatsModule } from './core/chats/chats.module';
+import { ParticipantsModule } from './core/participants/participants.module';
 
 @Module({
   imports: [
@@ -18,8 +22,15 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({}),
     UsersModule,
     AuthorizationModule,
+    TokensModule,
+    ChatsModule,
+    ParticipantsModule,
   ],
-  providers: [Logger],
+  providers: [Logger, AccessTokenMiddleware],
   exports: [Logger],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AccessTokenMiddleware).forRoutes('*');
+  }
+}
