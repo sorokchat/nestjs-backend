dc9fb5ddbafc0c86c458c7d62cd693476878fa5f
Sorokovsky <Sorokovskys@ukr.net>
2026-08-10 20:06:32 +0300

    fix: 409 response in register

--- src/core/authorization/access-token.middleware.ts
+++ src/core/authorization/access-token.middleware.ts
@@ -1,36 +1,33 @@
 import { NestMiddleware, Injectable } from '@nestjs/common';
 import { type Request, type Response } from 'express';
-import { AuthorizationService } from './authorization.service';
 import { TokensService } from '../tokens/tokens.service';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AccessTokenMiddleware implements NestMiddleware {
   constructor(
-    private readonly authorizationService: AuthorizationService,
     private readonly tokensService: TokensService,
     private readonly usersService: UsersService,
   ) {}
 
   public async use(
     request: Request,
-    response: Response,
+    _response: Response,
     next: () => void,
   ): Promise<void> {
-    const method: string = 'Bearer';
     const header = request.headers['authorization'];
-    if (!header) this.authorizationService.unauthorized(response, method);
+    if (!header) return next();
     const parsed = await this.tokensService.verifyAccessToken(
       header?.replace('Bearer ', ''),
     );
-    if (!parsed) this.authorizationService.unauthorized(response, method);
+    if (!parsed) return next();
     try {
       const user = await this.usersService.getBy({
         id: parsed.id ?? undefined,
       });
       request['user'] = user;
     } catch {
-      this.authorizationService.unauthorized(response, method);
+      next();
     }
     next();
   }
